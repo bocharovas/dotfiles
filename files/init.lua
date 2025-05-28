f5142342a7b6e8ef69670a1691edc9991e1c3810
Alexander Bocharov <puppaterraform@mail.ru>
2025-05-28 12:25:17 +0300

    Fix Vimwiki startup by adjusting configuration (#6)
    * fix(xfce4-session.xml): 🐛 modify SaveOnExit
    
      <property name="SaveOnExit" type="bool" value="true"/> to false
    
    * fix(init.lua): undefined adjust configuration for proper Vimwiki

--- files/init.lua
+++ files/init.lua
@@ -1,6 +1,11 @@
--- init.lua (перенос с packer.nvim на lazy.nvim)
+-- init.lua
 
--- Установка lazy.nvim
+vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true })
+
+vim.g.mapleader = " "
+
+-- Bootstrap lazy.nvim, если еще не установлен
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -8,21 +13,55 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
+    "--branch=stable", -- latest stable release
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Загрузка плагинов
+-- Настройка плагинов через lazy.nvim
 require("lazy").setup({
-  { "vimwiki/vimwiki" },
-  { "tbabej/taskwiki" },
+  {
+    "vimwiki/vimwiki",
+    init = function()
+      vim.g.vimwiki_list = {{
+        path = os.getenv("HOME") .. "/vimwiki/",
+        syntax = "markdown",
+        ext = ".md",
+        template_path = vim.fn.expand("~/vimwiki/templates/"),
+        template_default = "daily",
+        template_ext = ".md",
+      }}
+      vim.g.vimwiki_global_ext = 0
+    end,
+    config = function()
+      vim.api.nvim_set_keymap("n", "<leader>ww", ":VimwikiIndex<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<leader>wt", ":VimwikiMakeDiaryNote<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<leader>ws", ":VimwikiUISelect<CR>", { noremap = true, silent = true })
+    end,
+  },
+
+  {
+    "tbabej/taskwiki",
+    config = function()
+      vim.g.taskwiki_markdown = 1
+      vim.g.taskwiki_update_on_write = 1
+      vim.g.taskwiki_split_cmd = "botright vsplit"
+      vim.g.taskwiki_date_format = "%Y-%m-%d"
+    end,
+  },
 })
 
--- Конфигурация
-vim.cmd('syntax on')
+-- Включаем подсветку синтаксиса
+vim.cmd("syntax on")
 
+-- Включаем номера строк и относительные номера
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Задаём лидер (если ещё не установлен)
+
+-- Автозапуск скрипта tasks.sh при сохранении index.md
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "index.md",
   callback = function()
@@ -33,32 +72,8 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     else
       print("Скрипт не найден: " .. script_path)
     end
-  end
+  end,
 })
 
-vim.o.number = true
-vim.g.mapleader = ' '
-
-vim.g.vimwiki_list = {{
-  path = os.getenv("HOME") .. "/vimwiki/",
-  syntax = 'markdown',
-  ext = '.md',
-  template_path = vim.fn.expand('~/vimwiki/templates/'),
-  template_default = 'daily',
-  template_ext = '.md'
-}}
-
-vim.api.nvim_set_keymap('n', '<leader>ww', ':VimwikiIndex<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>wt', ':VimwikiMakeDiaryNote<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ws', ':VimwikiUISelect<CR>', { noremap = true, silent = true })
-
-vim.g.taskwiki_markdown = 1
-vim.g.taskwiki_update_on_write = 1
-vim.g.taskwiki_split_cmd = 'botright vsplit'
-vim.g.taskwiki_date_format = '%Y-%m-%d'
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.g.python3_host_prog = '/usr/bin/python3'
-vim.g.vimwiki_global_ext = 0
-
+-- Указываем python3 интерпретатор для плагинов, если нужно
+vim.g.python3_host_prog = "/usr/bin/python3"

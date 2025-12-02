1fb093ffbbdba716531d64709eef3f22dac5b425
bocharovas <82609104+bocharovas@users.noreply.github.com>
2025-12-02 12:10:35 +0300

    feat(files): add xprofile and http to ip

--- .cz-config.js
+++ .cz-config.js
@@ -12,7 +12,7 @@ module.exports = {
     { value: 'release',  name: 'release:  Create a release commit' }
   ],
 
-  scopes: ['src', 'git-status', 'sys', 'autostart', 'root', 'pic', 'commitizen', 'links.sh', 'githooks', 'files-crypt', 'bashrc', 'bash_aliases', 'gitconfig', 'dirmngr', 'scripts'],
+  scopes: ['src', 'files', 'git-status', 'sys', 'autostart', 'root', 'pic', 'commitizen', 'links.sh', 'githooks', 'files-crypt', 'bashrc', 'bash_aliases', 'gitconfig', 'dirmngr', 'scripts'],
 
   allowCustomScopes: true,
 
